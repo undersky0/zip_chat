c44e9ea39fc5c07f087ae158a800c2617b31c31f
Chris Oliver <excid3@gmail.com>
2024-10-16 10:55:15 -0500

    Remove duplicate solid_queue entry in case statement

--- lib/jumpstart/lib/jumpstart/job_processor.rb
+++ lib/jumpstart/lib/jumpstart/job_processor.rb
@@ -32,8 +32,6 @@ module Jumpstart
         "bundle exec que -q default -q mailers -q action_mailbox_incineration -q action_mailbox_routing -q active_storage_analysis -q active_storage_purge"
       when "good_job"
         "bundle exec good_job start"
-      when "solid_queue"
-        "bundle exec rake solid_queue:start"
       end
     end
   end
