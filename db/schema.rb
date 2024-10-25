822d386b4029e3762f196176ef96b79fbf96ea33
Mike Dalton <kcdragon@users.noreply.github.com>
2024-10-25 09:33:51 -0400

    Remove addresses from schema since the model was removed (#1260)
    The model was removed in https://github.com/jumpstart-pro/jumpstart-pro-rails/pull/1243

--- db/schema.rb
+++ db/schema.rb
@@ -96,21 +96,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_21_183620) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "addresses", force: :cascade do |t|
-    t.string "addressable_type", null: false
-    t.bigint "addressable_id", null: false
-    t.integer "address_type"
-    t.string "line1"
-    t.string "line2"
-    t.string "city"
-    t.string "state"
-    t.string "country"
-    t.string "postal_code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["addressable_type", "addressable_id"], name: "index_addresses_on_addressable"
-  end
-
   create_table "announcements", force: :cascade do |t|
     t.string "kind"
     t.string "title"
