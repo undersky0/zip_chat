6f47e5aaddc01a610182106c6cabe4ebeec61908
Chris Oliver <excid3@gmail.com>
2024-11-05 16:01:59 -0600

    Configure resources in Madmin

---
+++ config/initializers/madmin.rb
@@ -0,0 +1,7 @@
+Madmin.site_name = Jumpstart.config.application_name
+
+Madmin.menu.before_render do
+  add label: "Users & Accounts", position: 1
+  add label: "Payments", position: 2
+  add label: "Resources", position: 3
+end
